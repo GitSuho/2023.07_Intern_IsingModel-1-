b0d0d4f2f9ca28f559ed7c1a487b996bf57ba029
GitSuho <cjsuhod0518@dgist.ac.kr>
2023-07-13 13:50:24 +0900

    IM_ver4.0.0
    Update Ising Model based by Cluster Algorithm.
    cluster_Ising worked, but it doesn't work well.

--- plot_graph.py
+++ plot_graph.py
@@ -29,27 +29,27 @@ for file_name in file_list:
             y_means[i] *= -1
     # y_abs = np.abs(y_means)
 
-#fit function and curve fitting work
-    def fit_function(x, a, b, c):
-        x = np.asarray(x)
-        return a*(((x-b)/c)**(1/8)) # m~[(Tc-T)/Tc]^{1/8}
-    x_fit_data = []
-    for i in x_values:
-        if (i > 2.3):
-            continue
-        x_fit_data.append(i)
-    y_fit_data = y_means[len(y_means) - len(x_fit_data):]
-    fit_const, _ = curve_fit(fit_function, x_fit_data, y_fit_data, p0=[1, 2, 2])
-    x_fit_values = np.linspace(0.6, 2.3, 200)
+# #fit function and curve fitting work
+#     def fit_function(x, a, b, c, d):
+#         x = np.asarray(x)
+#         return a*(((x-b)/c)**(1/8))+d # m~[(Tc-T)/Tc]^{1/8}
+#     x_fit_data = []
+#     for i in x_values:
+#         if (i > 2.3):
+#             continue
+#         x_fit_data.append(i)
+#     y_fit_data = y_means[len(y_means) - len(x_fit_data):]
+#     fit_const, _ = curve_fit(fit_function, x_fit_data, y_fit_data, p0=[1, 2, -3, 0.1])
+#     x_fit_values = np.linspace(x_fit_data[-1], x_fit_data[0], 200)
 
 #plot graph
     h = plt.errorbar(x_values, y_means, yerr=y_stddevs, ecolor='red' , elinewidth=1)
     h.get_children()[1].set_linestyle('--')
     # plt.plot(x_values, y_abs, color = 'green')
-    plt.plot(x_fit_values, fit_function(x_fit_values, *fit_const), color = 'orange')
+    # plt.plot(x_fit_values, fit_function(x_fit_values, *fit_const), color = 'orange')
     plt.xlabel('T')
     plt.ylabel('<m>')
     plt.suptitle('IsingModel_graph', fontsize = 15)
     plt.title(file_name[6:-4] , fontsize = 10)
-    plt.savefig(f'{file_name[:-4]}.jpg', dpi=3000)
+    # plt.savefig(f'{file_name[:-4]}.jpg', dpi=3000)
     plt.show()
